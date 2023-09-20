0fa48b597f489e8975d51d604351a8a88ecd346e
justin-m-murillo <95903182+justin-m-murillo@users.noreply.github.com>
2023-09-20 13:26:59 -0700

    update Details styles

--- src/components/TodoItem/Details/styles.tsx
+++ src/components/TodoItem/Details/styles.tsx
@@ -3,7 +3,7 @@ const styles = {
   fieldTable: 'flex flex-row',
   fieldKey: 'flex flex-col items-end mr-2 text-sm text-gray-200 font-bold',
   fieldValue: 'flex flex-col items-start text-sm text-gray-200',
-  listItemDesc: 'mt-4 flex-1 text-white',
+  listItemDesc: 'mt-4 flex-1 text-white mx-10',
 }
 
 export default styles
