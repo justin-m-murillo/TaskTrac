f525f42f46fe5a5745b475ccaf03cca50615c70f
justin-m-murillo <95903182+justin-m-murillo@users.noreply.github.com>
2023-09-22 07:53:39 -0700

    disable useFetchTodos and remove dep array in useItemContainerHover

--- src/hooks/useFetchTodos/index.tsx
+++ src/hooks/useFetchTodos/index.tsx
@@ -1,10 +1,9 @@
 'use server'
-import { prisma } from '../../app/db'
 import { Todo } from '@/types/Todo'
 
 const useFetchTodos = async () => {
-  const todos: Todo[] = await prisma.todo.findMany()
-  return todos
+  //const todos: Todo[] = await prisma.todo.findMany()
+  //return todos
 }
 
 export default useFetchTodos
