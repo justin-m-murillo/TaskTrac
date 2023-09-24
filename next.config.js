35a283011b918ed102a5f95f3b7204dacef98eaf
justin-m-murillo <95903182+justin-m-murillo@users.noreply.github.com>
2023-09-24 09:54:21 -0700

    remove unnecessary configs

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    addDir: true,
-    serverActions: true,
-  }
-}
+const nextConfig = {}
 
 module.exports = nextConfig
