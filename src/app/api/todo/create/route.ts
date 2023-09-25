efe3a89b661d2cea4523db05888ce680e4d2bcf0
justin-m-murillo <murillo_justin@yahoo.com>
2023-09-24 18:05:46 -0700

    debug post api pending

--- src/app/api/todo/create/route.ts
+++ src/app/api/todo/create/route.ts
@@ -6,26 +6,29 @@ import { authOptions } from '../../auth/[...nextauth]/route';
 
 export async function POST(request: NextRequest) {
   const { title, description, location, due_date, bg_gradient } = await request.json();
-  console.log(title);
-  const session = await getServerSession(authOptions)
-  const userId = session?.user.id;
-  console.log('POST USERID',userId);
+  console.log('POST TITLE', title);
+  const session = await getServerSession(authOptions);
+  const user = await prisma.user.findUnique({
+    where: { email: session?.user?.email as string | undefined }
+  })
+  console.log('POST USERID', user?.id);
 
   try {
-    const todo = await prisma.todo.create({
-      data: {
-        title,
-        description,
-        location,
-        due_date: due_date ? new Date(due_date) : null,
-        bg_gradient,
-        created_at: new Date(),
-        updated_at: new Date(),
-        user: {
-          connect: { id: userId } // Replace '...' with the actual user ID
-        }
-      },
-    });
+    const todo = null
+  //   const todo = await prisma.todo.create({
+  //     data: {
+  //       title,
+  //       description,
+  //       location,
+  //       due_date: due_date ? new Date(due_date) : null,
+  //       bg_gradient,
+  //       created_at: new Date(),
+  //       updated_at: new Date(),
+  //       user: {
+  //         connect: { id: userId } // Replace '...' with the actual user ID
+  //       }
+  //     },
+  //   });
   
     return NextResponse.json({ message: "POST SUCCESS", todo });
   } catch (error) {
