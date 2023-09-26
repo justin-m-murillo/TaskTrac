e008d10a5aa675e6b4bef3355b2dad2a4f34c5bd
justin-m-murillo <95903182+justin-m-murillo@users.noreply.github.com>
2023-09-26 13:50:28 -0700

    change datetime fields in schema to timestamp

--- src/utils/getDateTime/index.tsx
+++ src/utils/getDateTime/index.tsx
@@ -1,7 +1,7 @@
 import { DateTime } from "ts-luxon"
 
 const getDateTime = (date: Date, is24HourTime: boolean) => {
-  console.log('getDateTime', date.toString());
+  console.log('getDateTime', date);
   return is24HourTime
     ? DateTime.fromISO(date.toString()).toFormat('DD - T')
     : DateTime.fromISO(date.toString()).toFormat('DD - t');
