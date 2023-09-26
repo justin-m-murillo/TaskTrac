cddb1680dd49792789960ed42c1f2d98272a1004
justin-m-murillo <95903182+justin-m-murillo@users.noreply.github.com>
2023-09-26 16:40:35 -0700

    test parse then format

--- src/utils/getDateTime/index.tsx
+++ src/utils/getDateTime/index.tsx
@@ -4,9 +4,8 @@ import formatISO from "date-fns/formatISO";
 const getDateTime = (date: Date, is24HourTime: boolean) => {
   const dateString = date.toString();
   const parsedDateTime = DateTime.fromFormat(dateString, "EEE MMM dd yyyy HH:mm:ss 'GMT'ZZZ (z)", { zone: 'UTC' });
-  const sliced = date.toString().slice(0, -5);
-  console.log('sliced dt', parsedDateTime);
-  return sliced;
+  console.log('parsed DateTime', parsedDateTime);
+  return parsedDateTime;
   // return is24HourTime
   //   ? DateTime.fromISO(date.toString() as string).toFormat('DD - T')
   //   : DateTime.fromISO(date.toString() as string).toFormat('DD - t');
