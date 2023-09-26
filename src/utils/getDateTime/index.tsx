7e4c234e4f42cf54a38d5eed8fb06eb3d1d21691
justin-m-murillo <95903182+justin-m-murillo@users.noreply.github.com>
2023-09-26 16:41:23 -0700

    add more logs

--- src/utils/getDateTime/index.tsx
+++ src/utils/getDateTime/index.tsx
@@ -2,6 +2,8 @@ import { DateTime } from "ts-luxon"
 import formatISO from "date-fns/formatISO";
 
 const getDateTime = (date: Date, is24HourTime: boolean) => {
+  console.log('Date object', date);
+  console.log('Date object to string', date.toString());
   const dateString = date.toString();
   const parsedDateTime = DateTime.fromFormat(dateString, "EEE MMM dd yyyy HH:mm:ss 'GMT'ZZZ (z)", { zone: 'UTC' });
   console.log('parsed DateTime', parsedDateTime);
