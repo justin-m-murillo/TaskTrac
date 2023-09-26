2c88e69d5daf88ce90b1c3358e7b94aadce10196
justin-m-murillo <95903182+justin-m-murillo@users.noreply.github.com>
2023-09-26 14:30:51 -0700

    test raw datetime string

--- src/utils/getDateTime/index.tsx
+++ src/utils/getDateTime/index.tsx
@@ -1,10 +1,13 @@
 import { DateTime } from "ts-luxon"
 
 const getDateTime = (date: Date, is24HourTime: boolean) => {
-  console.log('getDateTime', date);
-  return is24HourTime
-    ? DateTime.fromISO(date.toString()).toFormat('DD - T')
-    : DateTime.fromISO(date.toString()).toFormat('DD - t');
+  console.log('getDateTime DATE STRING', date.toString());
+  console.log('getDateTime DATE TO FORMAT', DateTime.fromISO(date.toString()).toFormat('DD - T'));
+  
+  return date.toString();
+  // return is24HourTime
+  //   ? DateTime.fromISO(date.toString()).toFormat('DD - T')
+  //   : DateTime.fromISO(date.toString()).toFormat('DD - t');
 }
 
 export default getDateTime
