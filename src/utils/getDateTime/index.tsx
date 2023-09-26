48230f4992622ccb9bca309c5f6a95adcc32ee7f
justin-m-murillo <95903182+justin-m-murillo@users.noreply.github.com>
2023-09-26 14:40:05 -0700

    test new parse method for datetime

--- src/utils/getDateTime/index.tsx
+++ src/utils/getDateTime/index.tsx
@@ -3,11 +3,14 @@ import { DateTime } from "ts-luxon"
 const getDateTime = (date: Date, is24HourTime: boolean) => {
   console.log('getDateTime DATE STRING', date.toString());
   console.log('getDateTime DATE TO FORMAT', DateTime.fromISO(date.toString()).toFormat('DD - T'));
-  
-  return date.toString();
-  // return is24HourTime
-  //   ? DateTime.fromISO(date.toString()).toFormat('DD - T')
-  //   : DateTime.fromISO(date.toString()).toFormat('DD - t');
+  const parsedDateTime = DateTime.fromFormat(date.toString(), "EEE MMM dd yyyy HH:mm:ss 'GMT'ZZZZZ (z)", { zone: 'UTC' })
+  console.log('parsedDateTime', parsedDateTime);
+  const formatISO = parsedDateTime.toISO();
+  console.log('formatISO', formatISO);
+  //return date.toString();
+  return is24HourTime
+    ? DateTime.fromISO(formatISO as string).toFormat('DD - T')
+    : DateTime.fromISO(formatISO as string).toFormat('DD - t');
 }
 
-export default getDateTime
+export default getDateTime;
