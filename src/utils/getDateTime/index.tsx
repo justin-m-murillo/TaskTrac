8d6586680c3358b9430a38911276a05e7d637476
justin-m-murillo <95903182+justin-m-murillo@users.noreply.github.com>
2023-09-26 16:24:34 -0700

    test sliced string

--- src/utils/getDateTime/index.tsx
+++ src/utils/getDateTime/index.tsx
@@ -1,16 +1,14 @@
 import { DateTime } from "ts-luxon"
+import formatISO from "date-fns/formatISO";
 
 const getDateTime = (date: Date, is24HourTime: boolean) => {
-  console.log('getDateTime DATE STRING', date.toString());
-  console.log('getDateTime DATE TO FORMAT', DateTime.fromISO(date.toString()).toFormat('DD - T'));
-  const parsedDateTime = DateTime.fromFormat(date.toString(), "EEE MMM dd yyyy HH:mm:ss 'GMT'ZZZZZ (z)", { zone: 'UTC' })
-  console.log('parsedDateTime', parsedDateTime);
-  const formatISO = parsedDateTime.toISO();
-  console.log('formatISO', formatISO);
-  //return date.toString();
-  return is24HourTime
-    ? DateTime.fromISO(formatISO as string).toFormat('DD - T')
-    : DateTime.fromISO(formatISO as string).toFormat('DD - t');
+  console.log('getDateTime Raw Date', date.toString().slice(0, -5));
+  const sliced = date.toString().slice(0, -5);
+  console.log('sliced dt', sliced);
+  return sliced;
+  // return is24HourTime
+  //   ? DateTime.fromISO(date.toString() as string).toFormat('DD - T')
+  //   : DateTime.fromISO(date.toString() as string).toFormat('DD - t');
 }
 
 export default getDateTime;
