c954f8101045551605f6443332b0a1badf775b8f
justin-m-murillo <95903182+justin-m-murillo@users.noreply.github.com>
2023-09-26 17:09:56 -0700

    convert to string for debugging

--- src/utils/getDateTime/index.tsx
+++ src/utils/getDateTime/index.tsx
@@ -7,7 +7,7 @@ const getDateTime = (date: Date, is24HourTime: boolean) => {
   const dateString = date.toString();
   const parsedDateTime = DateTime.fromFormat(dateString, "EEE MMM dd yyyy HH:mm:ss 'GMT'ZZZ (z)", { zone: 'UTC' });
   console.log('parsed DateTime', parsedDateTime);
-  return parsedDateTime;
+  return parsedDateTime.toString() as string;
   // return is24HourTime
   //   ? DateTime.fromISO(date.toString() as string).toFormat('DD - T')
   //   : DateTime.fromISO(date.toString() as string).toFormat('DD - t');
