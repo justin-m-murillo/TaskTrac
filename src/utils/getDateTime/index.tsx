fdd2c1f72d913ffe04f578fed00299849610798c
justin-m-murillo <95903182+justin-m-murillo@users.noreply.github.com>
2023-09-26 18:15:57 -0700

    testing timestamp type

--- src/utils/getDateTime/index.tsx
+++ src/utils/getDateTime/index.tsx
@@ -3,11 +3,8 @@ import formatISO from "date-fns/formatISO";
 
 const getDateTime = (date: Date, is24HourTime: boolean) => {
   console.log('Date object', date);
-  const dateString = date.toString();
-  console.log('Date string', dateString);
-  const parsedDateTime = DateTime.fromFormat(dateString, "EEE MMM dd yyyy HH:mm:ss 'GMT'ZZZ (ZZZZZ)", { zone: 'UTC' });
-  console.log('parsed DateTime', parsedDateTime);
-  return parsedDateTime.toString() as string;
+  
+  return "";
   // return is24HourTime
   //   ? DateTime.fromISO(date.toString() as string).toFormat('DD - T')
   //   : DateTime.fromISO(date.toString() as string).toFormat('DD - t');
