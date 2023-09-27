7aa0a045918c75cc6485b4f7f9c2ca40b8ae8367
justin-m-murillo <95903182+justin-m-murillo@users.noreply.github.com>
2023-09-26 18:47:01 -0700

    test extract date

--- src/utils/getDateTime/index.tsx
+++ src/utils/getDateTime/index.tsx
@@ -3,8 +3,13 @@ import formatISO from "date-fns/formatISO";
 
 const getDateTime = (date: Date, is24HourTime: boolean) => {
   console.log('Date object', date);
-  const iso = DateTime.fromJSDate(date).toISO();
-  console.log('iso', iso);
+  const newDate: Date = date;
+  console.log('iso', newDate.toISOString());
+  console.log('utc', newDate.toUTCString());
+  console.log('getDate', newDate.getDate);
+  console.log('getMonth', newDate.getMonth());
+  console.log('getHours', newDate.getHours());
+  console.log('getEpoch', newDate.getTime());
   return "";
   // return is24HourTime
   //   ? DateTime.fromISO(date.toString() as string).toFormat('DD - T')
