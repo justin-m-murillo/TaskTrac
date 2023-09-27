9135c1e536747a2662bec61ba5f1fcd0c163b910
justin-m-murillo <95903182+justin-m-murillo@users.noreply.github.com>
2023-09-26 18:41:10 -0700

    test iso from js object

--- src/utils/getDateTime/index.tsx
+++ src/utils/getDateTime/index.tsx
@@ -3,7 +3,8 @@ import formatISO from "date-fns/formatISO";
 
 const getDateTime = (date: Date, is24HourTime: boolean) => {
   console.log('Date object', date);
-  
+  const iso = DateTime.fromJSDate(date).toISO();
+  console.log('iso', iso);
   return "";
   // return is24HourTime
   //   ? DateTime.fromISO(date.toString() as string).toFormat('DD - T')
